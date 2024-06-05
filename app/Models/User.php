775ca404a207c2e526e7209af15bdb59c6a18c32
Tamar Sinauridze <sin165165@gmail.com>
2024-06-05 23:16:45 +0400

    feat: queue password reset emails

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\Auth\QueuedResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -43,5 +44,10 @@ class User extends Authenticatable implements MustVerifyEmail
 			'email_verified_at' => 'datetime',
 			'password'          => 'hashed',
 		];
+	}
+
+	public function sendPasswordResetNotification($token): void
+	{
+		$this->notify(new QueuedResetPassword($token));
 	}
 }
