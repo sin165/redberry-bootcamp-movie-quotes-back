aa64003acd761b7846c2ef04040c3a5152b0df92
Tamar Sinauridze <sin165165@gmail.com>
2024-06-07 17:59:28 +0400

    refactor: create migration for google id field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,6 @@ return new class extends Migration {
 	{
 		Schema::create('users', function (Blueprint $table) {
 			$table->id();
-			$table->string('google_id', 32)->unique()->nullable();
 			$table->string('name');
 			$table->string('email')->unique();
 			$table->timestamp('email_verified_at')->nullable();
